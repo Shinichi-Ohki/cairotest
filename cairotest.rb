5ccfe8753e7fda7b900951ee35bf1c87dbb6dcd5
Shinichi Ohki <auxin.one@gmail.com>
2015-04-13 02:27:41 +0900

    SVGで文字出力がおかしかったため、出力をPNGに戻した。文字を枠で囲った。

--- cairotest.rb
+++ cairotest.rb
@@ -3,6 +3,8 @@ require 'cairo'
 format = Cairo::FORMAT_ARGB32
 width = 300
 height = 200
+merginV = 5
+merginH = 5
 
 if ARGV[0]==nil then
 	ExitStatus = 0
@@ -11,10 +13,12 @@ if ARGV[0]==nil then
 end
 
 SVGfilename = ARGV[0]+".svg"
+PNGfilename = ARGV[0]+".png"
 
-puts(SVGfilename)
+puts(PNGfilename)
 
-surface = Cairo::SVGSurface.new(SVGfilename,width,height)
+#surface = Cairo::SVGSurface.new(SVGfilename,width,height)
+surface = Cairo::ImageSurface.new(format,width,height)
 context = Cairo::Context.new(surface)
 
 # 背景
@@ -23,11 +27,29 @@ context.fill do
 	context.rectangle(0, 0, width, height)
 end
 
+# 角丸四角
 context.fill do
 	context.set_source_rgb(0, 0, 0) # 黒
 	context.rounded_rectangle(10, 10, width-20, height-20,10)
 	context.stroke
 end
 
-context.target.finish
-#context.target.write_to_png("cairotest.png")
+# 文字
+context.set_source_rgb(0, 0, 0) # 黒
+context.select_font_face("Helvetica",nil,"bold")
+context.set_font_size(20)
+context.move_to(20,100)
+str = "test"
+context.set_line_width(3.0)
+context.show_text(str)
+context.fill do
+	context.set_source_rgb(0, 0, 0) # 黒
+	width = context.text_extents(str).width
+	height = context.text_extents(str).height
+	context.rounded_rectangle(20-merginH, 100-height-merginV, width+merginH*2, height+merginV*2,5)
+	context.set_line_width(4.0)
+	context.stroke
+end
+
+#context.target.finish
+context.target.write_to_png(PNGfilename)
